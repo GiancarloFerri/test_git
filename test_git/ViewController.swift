8b176e54c127bf264d517d918ef6adb198d396a9
git <git@LD140-53-06286.local>
2017-10-16 09:30:57 -0400

    ligne 15

--- test_git/ViewController.swift
+++ test_git/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hey guys! Ligne 15 here! Today we're gonna play FNAF")
         // Do any additional setup after loading the view, typically from a nib.
         print("ViewController:vieweDidLoad")
         afficher("NANIIII", fois: 5)
